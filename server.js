88ffa83ab78920cf918bb6231db26a4ca07bddd7
Justin Ndwiga <ndwigajustin@gmail.com>
2019-01-25 19:20:43 +0300

    Add prod server

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-// the __dirname is the current directory from where the script is running
+// the __dirname is the current entry directory 
 app.use(express.static(__dirname));
 
-// send the user to index html page inspite of the url
+// use index.html as the access point
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
