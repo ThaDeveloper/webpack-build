5670af0590c661d7ee9becc3ea25cd9e67f1802c
Justin Ndwiga <ndwigajustin@gmail.com>
2019-01-25 16:22:34 +0300

    Setup webpack, babel and react with hotmodule

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './views/Home';
+
+const App = () => {
+  return (
+    <div className="main-app">
+       <Home />
+    </div>
+  )
+}
+export default  App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="container">
+        <div className="row">
+          <div className="col md-12">
+            <h1>HOME PLACE</h1>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home;
